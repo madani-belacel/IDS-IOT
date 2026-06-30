988103f89118462c0bf6582d8ce645a41d01f971
Madani Belacel <madani.belacel@univ-mosta.dz>
2026-06-30 17:45:09 +0100

    Mise à jour : corrections diverses - 2026-06-30 17:45

---
+++ push.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd ~/contiki-ng/examples/IDS_IOT
+git add .
+git commit -m "Update - $(date '+%Y-%m-%d %H:%M')"
+git push https://madani-belacel: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@github.com/madani-belacel/IDS-IOT.git main
